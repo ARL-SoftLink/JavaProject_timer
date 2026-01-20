f8b7216d45152d61fc3256a4e4e5a126af287734
Softlink <arielgiovanni90@gmail.com>
2026-01-20 14:07:05 -0600

    practica con un archivo.py

--- Documents/Sala-estudio/Departamento programacion/Ejercicios JAVA/example.java
+++ Documents/Sala-estudio/Departamento programacion/Ejercicios JAVA/example.java
@@ -1,4 +1,9 @@
+Public class aprendizaje {
+    public aprendizaje(){
+        InitializeComponents();
+    }//create a constructor
 public static void main(String []args)
 {
     System.out.println("HelloWorld");
+}
 }
