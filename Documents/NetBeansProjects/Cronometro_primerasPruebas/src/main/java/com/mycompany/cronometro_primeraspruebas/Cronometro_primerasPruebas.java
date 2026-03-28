12e1ccf12f2cf9cc92fdd7d26d3e6d39d363b570
Softlink <arielgiovanni90@gmail.com>
2026-03-28 17:12:52 -0600

    -Se añadio la logica correcta para las horas -Se corrigieron los errores de unidades con centesimas, segundos y minutos -se eliminaron lineas de codigo inutilizables

--- Documents/NetBeansProjects/Cronometro_primerasPruebas/src/main/java/com/mycompany/cronometro_primeraspruebas/Cronometro_primerasPruebas.java
+++ Documents/NetBeansProjects/Cronometro_primerasPruebas/src/main/java/com/mycompany/cronometro_primeraspruebas/Cronometro_primerasPruebas.java
@@ -16,38 +16,40 @@ public class Cronometro_primerasPruebas {
     
 
     public static void main(String[] args) {
-        Timer  Mytemp = new Timer();
+        System.out.println("Cronometro Iniciado");
+        Timer  myTemp = new Timer();
         TimerTask task =new TimerTask() {
             int centesimas=0;
             int segundos =0;
             int minutos =0;
+            int horas = 0;
+            
             @Override
             public void run() {
-                if(centesimas <=99) //condicion clave // \n centesimas == 0 || 
+                
+                if(centesimas <=98) //condicion clave 
                 {
                     centesimas++; //incrementar en 1 a centesimas
-                    System.out.println(centesimas +" centesimas");
+                    //System.out.println(centesimas +" centesimas");                    
                 }
                 else{
-                    centesimas=centesimas%100; //reiniciar centesimas
+                    centesimas=centesimas%99; //reiniciar centesimas
                     segundos++; // cuando centesimas alcanza su maximo se reinicia y suma valor 01 a los minutos
                     System.out.println(segundos+"======================================== segundos=====================================");// mostrar por consola 
-                    if(segundos==60){
-                        segundos=segundos%60;
+                    if(segundos == 59){
+                        segundos = segundos%59;
                         minutos++;
                         System.out.println(minutos +"========minutos=========================minutos==============================minutos============");
-                    }else{
-                        
                     }
+                    if(minutos==59){
+                        minutos=minutos%59;//reiniciar a cero
+                        horas++;
+                        System.out.print(horas+" Horas");  
+                    }   
                 }
-                /**
-                if(segundos==60)
-                {
-                    System.out.println("============ se han cumplido los 60 segundos, sumamos 1 minuto ============");
-                }**/
             }
         };
         
-        Mytemp.scheduleAtFixedRate(task, 0, 10);
+        myTemp.scheduleAtFixedRate(task, 0, 10);
     }
 }
