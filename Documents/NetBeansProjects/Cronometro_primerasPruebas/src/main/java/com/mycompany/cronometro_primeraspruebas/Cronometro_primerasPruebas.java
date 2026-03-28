c852b66a5664d88fd1caf3866df3aec545449162
Softlink <arielgiovanni90@gmail.com>
2026-03-27 23:34:11 -0600

    Initial commit: prueba y error de centesimas y segundos

---
+++ Documents/NetBeansProjects/Cronometro_primerasPruebas/src/main/java/com/mycompany/cronometro_primeraspruebas/Cronometro_primerasPruebas.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cronometro_primeraspruebas;
+
+/**
+ *
+ * @author MINEDUCYT
+ */
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Cronometro_primerasPruebas {
+    
+    
+
+    public static void main(String[] args) {
+        Timer  Mytemp = new Timer();
+        TimerTask task =new TimerTask() {
+            int centesimas=0;
+            int segundos =0;
+            int minutos =0;
+            @Override
+            public void run() {
+                if(centesimas <=99) //condicion clave // \n centesimas == 0 || 
+                {
+                    centesimas++; //incrementar en 1 a centesimas
+                    System.out.println(centesimas +" centesimas");
+                }
+                else{
+                    centesimas=centesimas%100; //reiniciar centesimas
+                    segundos++; // cuando centesimas alcanza su maximo se reinicia y suma valor 01 a los minutos
+                    System.out.println(segundos+"======================================== segundos=====================================");// mostrar por consola 
+                }
+                /**
+                if(segundos==60)
+                {
+                    System.out.println("============ se han cumplido los 60 segundos, sumamos 1 minuto ============");
+                }**/
+            }
+        };
+        
+        Mytemp.scheduleAtFixedRate(task, 0, 10);
+    }
+}
